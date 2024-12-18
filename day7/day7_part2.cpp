112d79d52417c85fbf0d6d217eae196cf5b890b9
Ashley <ashleyoftexas@gmail.com>
2024-12-17 23:23:36 -0600

    day 7

---
+++ day7/day7_part2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <regex>
+#include <vector>
+
+using namespace std;
+
+vector<int> parse(string s)
+{
+    stringstream ss(s);
+    vector<int> vec;
+    int num;
+
+    while (ss.good())
+    {
+        ss >> num;
+        vec.push_back(num);
+    }
+    return vec;
+}
+
+bool dfs(vector<int> vec, int i, long sum, long total)
+{
+    if (i == vec.size() && sum == total)
+        return true;
+    if (i >= vec.size())
+        return false;
+    if (dfs(vec, i + 1, sum + vec[i], total))
+        return true;
+    if (dfs(vec, i + 1, sum * vec[i], total))
+        return true;
+    return dfs(vec, i + 1, stol(to_string(sum) + to_string(vec[i])), total);
+}
+
+int main()
+{
+    ifstream file("input.txt");
+    string line;
+
+    regex pattern("(\\d+)\\: (.+)");
+    smatch match;
+
+    long ans = 0;
+
+    while (getline(file, line))
+    {
+        regex_search(line, match, pattern);
+        long total = stol(match[1]);
+        string nums = match[2];
+        vector<int> vec = parse(nums);
+
+        if (dfs(vec, 1, vec[0], total))
+            ans += total;
+    }
+    cout << ans << endl;
+    return 0;
+}
